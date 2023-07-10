6d825e406984eaee2497ad2b688fb95da417195e
Mikasa03 <106001202+Mikasa03@users.noreply.github.com>
2023-07-10 19:47:13 +0530

    Add files via upload

---
+++ partials-reserve/menu.php
@@ -0,0 +1,25 @@
+<?php include('/Users/91944/Desktop/apache/htdocs/olive-kitchen/food-order/config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Reservation Form</title>
+        <link rel="icon" type="image/x-icon" href="logo_olive.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="food-order/css/style.css">
+    </head>
+    <body>
+       <section class="navbar">
+            <div class="container">
+                <ul><li><a href="#">Home</a></li></ul>
+                <div class="menu text-right">
+                     <ul>
+                    
+                        <li><a href="<?php echo SITEURL;?>admin/login-cus.php">Login</a></li>
+                        <li><a href="<?php echo SITEURL;?>admin/manage-customer.php">Register</a></li>
+                </div>
+                        
+            </ul>
+        </section>
