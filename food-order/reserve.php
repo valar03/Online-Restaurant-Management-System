c9a761ca154e198c2489a2626b3e142db4a3964f
Valarmathi B <106001202+valar03@users.noreply.github.com>
2023-07-10 23:07:57 +0530

    Update reserve.php

--- food-order/reserve.php
+++ food-order/reserve.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
 
-    <link rel="icon" type="image/x-icon" href="http://localhost:8080/olive-kitchen//logo_olive.jpg">
+    <link rel="icon" type="image/x-icon" href="../logo_olive.jpg">
 
     <!-- Link our CSS file -->
     <link rel="stylesheet" href="css/style.css">
