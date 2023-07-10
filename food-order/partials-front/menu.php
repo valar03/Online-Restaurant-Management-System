d140fd1aba64badf722c459af802b40c7fcd4e30
Valarmathi B <106001202+valar03@users.noreply.github.com>
2023-07-10 23:01:47 +0530

    Update menu.php

--- food-order/partials-front/menu.php
+++ food-order/partials-front/menu.php
@@ -7,7 +7,7 @@
     <!-- Important to make website responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
-    <link rel="icon" type="image/x-icon" href="http://localhost:8080/olive-kitchen/logo_olive.jpg">
+    <link rel="icon" type="image/x-icon" href="../../logo_olive.jpg">
 
     <!-- Link our CSS file -->
     <link rel="stylesheet" href="css/style.css">
